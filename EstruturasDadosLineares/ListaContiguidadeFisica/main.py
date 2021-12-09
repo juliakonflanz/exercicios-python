c29bbbf12ea38581bb276704aa51c4316ca4a521
Júlia Konflanz <84208213+juliakonflanz@users.noreply.github.com>
2021-12-09 17:33:59 -0300

    Update main.py

--- EstruturasDadosLineares/ListaContiguidadeFisica/main.py
+++ EstruturasDadosLineares/ListaContiguidadeFisica/main.py
@@ -1,73 +1,46 @@
-class Lista:
-    def __init__(self, max):
-        self.max = max
-        self.vetor = [None] * self.max
-        self.ini = -1
-        self.fim = -1
+from produto import *
+from lista import *
 
-    def __repr__(self):
-        string = "[ini "
-        for i in range(self.ini, self.fim + 1):
-            string = string + " -> " + str(self.vetor[i])
-        return string + "]\n"
+produtoA = produto("1005", 122020)
+produtoB = produto("8491", 112021)
+produtoC = produto("2254", 102021)
 
-    def vazia(self):
-        return self.ini == -1 or self.fim == -1
+print("\n\tCRIANDO LISTA")
+lista = Lista(7)
+if (lista.inserir(1, produtoA)):
+    print(lista)
+if (lista.inserir(2, produtoB)):
+    print(lista)
+if (lista.inserir(3, produtoC)):
+    print(lista)
 
-    def tamanho(self):
-        if self.vazia():
-            return 0
-        else:
-            return self.fim - self.ini + 1
+print("\n\tINSERINDO PRODUTOS NA LISTA")
+lista.inserir(4,produtoC)
+print(lista)
+lista.inserir(4,produtoC)
+print(lista)
+lista.inserir(4,produtoC)
+print(lista)
+lista.inserir(4,produtoC)
+print(lista)
 
-    def inserir(self, posicao, dado):
-        if ((self.ini != 0 or self.fim != self.max - 1) and posicao > 0 and posicao <= self.tamanho()+1):
-            if (self.vazia()):
-               self.ini = self.max // 2
-               self.fim = self.max // 2
-            elif (self.fim != self.max - 1):
-                for i in range(self.fim, self.ini + posicao -2, -1):
-                    self.vetor[i+1] = self.vetor[i]
-                self.fim = self.fim + 1
-            else:
-                for i in range(self.ini, self.ini + posicao):
-                    self.vetor[i-1] = self.vetor[i]
-                self.ini = self.ini - 1
-            self.vetor[self.ini + posicao - 1] = dado
-            return True
-        else:
-            return False
+print("\n\tBUSCANDO DADOS DO PRODUTO")
+meuproduto = lista.buscar(1)
+print(meuproduto)
 
-    def remover(self, posição):
-        if((posição > self.fim - self.ini) or (posição < 0)):
-            print("Posição invalida.")
-        else:
-            self.vetor[self.ini + posição] = None
-
-    def buscar(self, posicao):
-        if not self.vazia():
-            return self.vetor[self.ini + posicao - 1]
-            
-    def destruir(self):
-        self.vetor = None
-        self.ini = 0
-        self.fim = -1
-        self.tamanho = 0
-    
-    def inserirOtimizado(self, posicao, dado):
-        if ((self.ini != 0 or self.fim != self.max - 1) and posicao > 0 and posicao <= self.tamanho()+1):
-            if (self.vazia()):
-               self.ini = self.max // 2
-               self.fim = self.max // 2
-            elif (self.ini == 0 or(posicao > self.tamanho() // 2 and self.fim != self.max -1)):
-                for i in range(self.fim, self.ini + posicao -2, -1):
-                    self.vetor[i+1] = self.vetor[i+1]
-                self.fim = self.fim + 1
-            else:
-                for i in range(self.ini, self.ini + posicao - 1):
-                    self.vetor[i-1] = self.vetor[i]
-                self.ini = self.ini - 1
-            self.vetor[self.ini + posicao - 1] = dado
-            return True
-        else:
-            return False
+print("\n\tINSERÇÃO OTIMIZADA")
+lista2 = Lista(7)
+if (lista2.inserirOtimizado(1,produtoA)):
+    print(repr(lista2))
+if (lista2.inserirOtimizado(2,produtoB)):
+    print(lista2)
+if (lista2.inserirOtimizado(3,produtoC)):
+    print(lista2)
+if (lista2.inserirOtimizado(1,produtoB)):
+    print(lista2)
+if (lista2.inserirOtimizado(2,produtoC)):
+    print(lista2)
+if (lista2.inserirOtimizado(4,produtoC)):
+    print(lista2)
+if (lista2.inserirOtimizado(5,produtoC)):
+    print(lista2)
