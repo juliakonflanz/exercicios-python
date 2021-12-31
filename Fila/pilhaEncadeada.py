ae08cb37afbb4a989b63a8f384e8a4b444acf483
Júlia Konflanz <84208213+juliakonflanz@users.noreply.github.com>
2021-12-31 08:37:24 -0300

    Add files via upload

---
+++ Fila/pilhaEncadeada.py
@@ -0,0 +1,47 @@
+class Nodo:
+    def __init__(self, dado):
+        self.dado = dado
+        self.prox = None
+
+    def __repr__(self):
+        return "%s -> %s" % (self.dado, self.prox)
+
+
+class pilha_enc:
+    def __init__(self):
+        self.topo = None
+    
+    def __repr__(self):
+        return "[" + str(self.topo) + "]"
+
+    def vazia(self):
+        if self.topo is None:
+           return True
+    
+    def imprime(self): 
+        conteudo = self.topo 
+        if conteudo is None:
+            print("A lista não tem elementos.")
+        while conteudo: 
+            print(conteudo.dado)
+            conteudo = conteudo.prox
+    
+    def empilhar(self, dado):
+        novo = Nodo(dado)
+        if (not self.vazia()):
+            novo.prox = self.topo
+        self.topo = novo
+
+    def excluir(self):
+        if (not self.vazia()):
+            aux = self.topo
+            self.topo = aux.prox
+            del aux
+    
+    def consultar(self):
+        if (not self.vazia()):
+            return self.topo.dado
+
+    def destruir(self):
+        while (not self.vazia()):
+            self.excluir()
